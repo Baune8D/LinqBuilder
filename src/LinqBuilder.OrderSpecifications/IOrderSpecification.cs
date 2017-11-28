566e3c77f388069c004eb0f77f5028ef1ab5f961
Morten Larsen <baunegaard@live.dk>
2017-11-28 10:12:48 +0100

    Add skip take

--- src/LinqBuilder.OrderSpecifications/IOrderSpecification.cs
+++ src/LinqBuilder.OrderSpecifications/IOrderSpecification.cs
@@ -3,5 +3,7 @@
     public interface IOrderSpecification<T>
     {
         ICompositeSpecification<T> ThenBy(OrderSpecification<T> other);
+        ICompositeSpecification<T> Skip(int count);
+        ICompositeSpecification<T> Take(int count);
     }
 }
