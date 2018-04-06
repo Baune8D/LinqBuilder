5008e09566329a1eac6c47ef4285fc4b331a4540
Morten Larsen <baunegaard@live.dk>
2018-04-06 02:37:20 +0200

    Add more extension methods

--- src/LinqBuilder.EFCore/EntityFrameworkCoreExtensions.cs
+++ src/LinqBuilder.EFCore/EntityFrameworkCoreExtensions.cs
@@ -19,5 +19,40 @@ namespace LinqBuilder.EFCore
             if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
             return await query.AllAsync(specification.AsExpression());
         }
+
+        public static async Task<int> CountAsync<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+            where TEntity : class
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return await query.CountAsync(specification.AsExpression());
+        }
+
+        public static async Task<TEntity> FirstAsync<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+            where TEntity : class
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return await query.FirstAsync(specification.AsExpression());
+        }
+
+        public static async Task<TEntity> FirstOrDefaultAsync<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+            where TEntity : class
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return await query.FirstOrDefaultAsync(specification.AsExpression());
+        }
+
+        public static async Task<TEntity> SingleAsync<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+            where TEntity : class
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return await query.SingleAsync(specification.AsExpression());
+        }
+
+        public static async Task<TEntity> SingleOrDefaultAsync<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+            where TEntity : class
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return await query.SingleOrDefaultAsync(specification.AsExpression());
+        }
     }
 }
--- src/LinqBuilder/LinqExtensions.cs
+++ src/LinqBuilder/LinqExtensions.cs
@@ -40,5 +40,77 @@ namespace LinqBuilder
             if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
             return collection.All(specification.AsFunc());
         }
+
+        public static int Count<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.Count(specification.AsExpression());
+        }
+
+        public static int Count<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.Count(specification.AsFunc());
+        }
+
+        public static long LongCount<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.LongCount(specification.AsExpression());
+        }
+
+        public static long LongCount<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.LongCount(specification.AsFunc());
+        }
+
+        public static TEntity First<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.First(specification.AsExpression());
+        }
+
+        public static TEntity First<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.First(specification.AsFunc());
+        }
+
+        public static TEntity FirstOrDefault<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.FirstOrDefault(specification.AsExpression());
+        }
+
+        public static TEntity FirstOrDefault<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.FirstOrDefault(specification.AsFunc());
+        }
+
+        public static TEntity Single<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.Single(specification.AsExpression());
+        }
+
+        public static TEntity Single<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.Single(specification.AsFunc());
+        }
+
+        public static TEntity SingleOrDefault<TEntity>(this IQueryable<TEntity> query, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return query.SingleOrDefault(specification.AsExpression());
+        }
+
+        public static TEntity SingleOrDefault<TEntity>(this IEnumerable<TEntity> collection, ISpecification<TEntity> specification)
+        {
+            if (specification == null) throw Exceptions.SpecificationCannotBeNull(nameof(specification));
+            return collection.SingleOrDefault(specification.AsFunc());
+        }
     }
 }
