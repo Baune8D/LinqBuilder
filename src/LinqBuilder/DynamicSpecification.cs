08a4985ab28f5838310cea97a219a9ddece0d752
Morten Larsen <baunegaard@live.dk>
2018-05-24 12:01:52 +0200

    Add dynamic specification

---
+++ src/LinqBuilder/DynamicSpecification.cs
@@ -0,0 +1,54 @@
+﻿namespace LinqBuilder
+{
+    public class DynamicSpecification<TEntity, TValue> : Specification<TEntity>
+        where TEntity : class
+    {
+        public TValue Value { get; private set; }
+
+        public ISpecification<TEntity> Set(TValue value)
+        {
+            Value = value;
+            return this;
+        }
+    }
+
+    public class DynamicSpecification<TEntity, TValue1, TValue2> : Specification<TEntity>
+        where TEntity : class
+    {
+        public TValue1 Value1 { get; private set; }
+        public TValue2 Value2 { get; private set; }
+
+        public ISpecification<TEntity> Set(TValue1 value1, TValue2 value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+            return this;
+        }
+    }
+
+    public class DynamicSpecification<TEntity, TValue1, TValue2, TValue3> : DynamicSpecification<TEntity, TValue1, TValue2>
+        where TEntity : class
+    {
+        public TValue3 Value3 { get; private set; }
+
+        public ISpecification<TEntity> Set(TValue1 value1, TValue2 value2, TValue3 value3)
+        {
+            Set(value1, value2);
+            Value3 = value3;
+            return this;
+        }
+    }
+
+    public class DynamicSpecification<TEntity, TValue1, TValue2, TValue3, TValue4> : DynamicSpecification<TEntity, TValue1, TValue2, TValue3>
+        where TEntity : class
+    {
+        public TValue4 Value4 { get; private set; }
+
+        public ISpecification<TEntity> Set(TValue1 value1, TValue2 value2, TValue3 value3, TValue4 value4)
+        {
+            Set(value1, value2, value3);
+            Value4 = value4;
+            return this;
+        }
+    }
+}
