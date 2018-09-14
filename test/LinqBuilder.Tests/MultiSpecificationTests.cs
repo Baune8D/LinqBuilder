d1442b9313661d7c2d24db4c3a734937f7164fd9
Morten Larsen <baunegaard@live.dk>
2018-09-14 15:52:44 +0200

    Add tests for MultiSpecification

---
+++ test/LinqBuilder.Tests/MultiSpecificationTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using LinqBuilder.Tests.TestHelpers;
+using Shouldly;
+using Xunit;
+
+namespace LinqBuilder.Tests
+{
+    public class MultiSpecificationTests
+    {
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification2_Entity()
+        {
+            new MultiEntitySpecification2()
+                .For<Entity>()
+                .IsSatisfiedBy(new Entity { Value1 = 1 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification2_Entity2()
+        {
+            new MultiEntitySpecification2()
+                .For<Entity2>()
+                .IsSatisfiedBy(new Entity2 { Value2 = 2 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification2_ShouldThrowException()
+        {
+            Should.Throw<Exception>(() =>new MultiEntitySpecification2()
+                .For<Entity3>()
+                .IsSatisfiedBy(new Entity3()));
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification3_Entity()
+        {
+            new MultiEntitySpecification3()
+                .For<Entity>()
+                .IsSatisfiedBy(new Entity { Value1 = 1 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification3_Entity2()
+        {
+            new MultiEntitySpecification3()
+                .For<Entity2>()
+                .IsSatisfiedBy(new Entity2 { Value2 = 2 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification3_Entity3()
+        {
+            new MultiEntitySpecification3()
+                .For<Entity3>()
+                .IsSatisfiedBy(new Entity3 { Value3 = 3 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification3_ShouldThrowException()
+        {
+            Should.Throw<Exception>(() => new MultiEntitySpecification3()
+                .For<Entity4>()
+                .IsSatisfiedBy(new Entity4()));
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification4_Entity()
+        {
+            new MultiEntitySpecification4()
+                .For<Entity>()
+                .IsSatisfiedBy(new Entity { Value1 = 1 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification4_Entity2()
+        {
+            new MultiEntitySpecification4()
+                .For<Entity2>()
+                .IsSatisfiedBy(new Entity2 { Value2 = 2 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification4_Entity3()
+        {
+            new MultiEntitySpecification4()
+                .For<Entity3>()
+                .IsSatisfiedBy(new Entity3 { Value3 = 3 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification4_Entity4()
+        {
+            new MultiEntitySpecification4()
+                .For<Entity4>()
+                .IsSatisfiedBy(new Entity4 { Value4 = 4 });
+        }
+
+        [Fact]
+        public void IsSatisfiedBy_MultiEntitySpecification4_ShouldThrowException()
+        {
+            Should.Throw<Exception>(() => new MultiEntitySpecification4()
+                .For<Entity5>()
+                .IsSatisfiedBy(new Entity5()));
+        }
+    }
+}
---
+++ test/LinqBuilder.Tests/TestHelpers/Entity3.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqBuilder.Tests.TestHelpers
+{
+    public class Entity3
+    {
+        public int Value1 { get; set; }
+
+        public int Value2 { get; set; }
+
+        public int Value3 { get; set; }
+
+        public int Value4 { get; set; }
+    }
+}
