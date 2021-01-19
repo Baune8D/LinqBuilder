7f458bb79b8d35d12582932417f97ec24eb9f362
Morten Larsen <baunegaard@live.dk>
2021-01-19 13:45:05 +0100

    Add assembly file versions

--- src/EF6/LinqBuilder.EF6/Properties/AssemblyInfo.cs
+++ src/EF6/LinqBuilder.EF6/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 using System;
+using System.Reflection;
 
 [assembly:CLSCompliant(true)]
+
+// These attributes will be patched by GitVersion
+[assembly: AssemblyVersion("")]
+[assembly: AssemblyFileVersion("")]
+[assembly: AssemblyInformationalVersion("")]
